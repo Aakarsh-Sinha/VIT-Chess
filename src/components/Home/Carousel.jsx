e4cfa93d9c5f622440bc7535afe9d7bc13dc5d49
Aakarsh Sinha <aakarshsinha.in@gmail.com>
2024-03-18 15:00:09 +0530

    Home Issue Resolved

--- src/components/Home/Carousel.jsx
+++ src/components/Home/Carousel.jsx
@@ -15,8 +15,8 @@ function Carousel_Home() {
             height="650px"
           ></img>
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>South Zonals 2024</h3>
+            <p>Third position</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -27,8 +27,8 @@ function Carousel_Home() {
             height="650px"
           ></img>
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>VIT South Zonals Teams</h3>
+            <p>Vellore - Left AP - Right</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -39,10 +39,8 @@ function Carousel_Home() {
             height="650px"
           ></img>
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3>CMC Pegasus 2023</h3>
+            <p>Team - First Individual - First</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Carousel_Home from "../components/Home/Carousel";
 import Quote from "../components/Home/Quote";
 import Card1 from "../components/Home/Card1";
 import ResponsiveExample from "../components/Home/Table";
+import Footer from "../components/Home/Footer";
 
 const Home = () => {
   return (
