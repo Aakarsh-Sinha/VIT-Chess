d5becf3e2a7658ede69a3f9b26bbfd09742c7a50
Kalki Eshwar D <kalkieshward@gmail.com>
2024-03-18 11:23:15 +0530

    Added link column and additional design to playoff rounds

--- src/components/NavbarBootstrap.jsx
+++ src/components/NavbarBootstrap.jsx
@@ -22,7 +22,7 @@ function NavbarBoot() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
+            <Nav.Link as={Link} to="/home">
               Home
             </Nav.Link>
             <Nav.Link as={Link} to="/about">
--- src/pages/vitcl/VITCLSchedule.jsx
+++ src/pages/vitcl/VITCLSchedule.jsx
@@ -7,68 +7,90 @@ const VITCLSchedule = () => {
         <div class="title">Round number</div>
         <div class="title">Teams participating</div>
         <div class="title">Blitz Arena Time</div>
+        <div class="title">Blitz Arena Link</div>
         <div class="title">Bullet Arena Time</div>
+        <div class="title">Blitz Arena Link</div>
         {/* Begin schedule */}
         <div class="date">March 18, 2024</div>
         <div class="round">Round 1.1</div>
         <div class="teams">Anand, Karpov, Kasparov, Hikaru</div>
         <div class="blitz-time">10:00 AM - 10:30 AM</div>
+        <div class="blitz-link">Not generated</div>
         <div class="bullet-time">10:45 AM - 11:00 AM</div>
-
+        <div class="bullet-link">Not generated</div>
+      
         <div class="date">March 18, 2024</div>
         <div class="round">Round 1.2</div>
         <div class="teams">MVL, Humpy, Carlsen, Fischer</div>
         <div class="blitz-time">10:00 AM - 10:30 AM</div>
+        <div class="blitz-link">Not generated</div>
         <div class="bullet-time">10:45 AM - 11:00 AM</div>
+        <div class="bullet-link">Not generated</div>
       
         <div class="date">March 19, 2024</div>
         <div class="round">Round 2.1</div>
         <div class="teams">Anand, Kasparov, Karpov, Hikaru</div>
         <div class="blitz-time">10:00 AM - 10:30 AM</div>
+        <div class="blitz-link">Not generated</div>
         <div class="bullet-time">10:45 AM - 11:00 AM</div>
-
+        <div class="bullet-link">Not generated</div>
+      
         <div class="date">March 19, 2024</div>
         <div class="round">Round 2.2</div>
         <div class="teams">MVL, Carlsen, Humpy, Fischer</div>
         <div class="blitz-time">10:00 AM - 10:30 AM</div>
+        <div class="blitz-link">Not generated</div>
         <div class="bullet-time">10:45 AM - 11:00 AM</div>
+        <div class="bullet-link">Not generated</div>
       
         <div class="date">March 20, 2024</div>
         <div class="round">Round 3.1</div>
         <div class="teams">Anand, MVL, Karpov, Humpy</div>
         <div class="blitz-time">10:00 AM - 10:30 AM</div>
+        <div class="blitz-link">Not generated</div>
         <div class="bullet-time">10:45 AM - 11:00 AM</div>
-
+        <div class="bullet-link">Not generated</div>
+      
         <div class="date">March 20, 2024</div>
         <div class="round">Round 3.2</div>
         <div class="teams">Kasparov, Carlsen, Hikaru, Fischer</div>
         <div class="blitz-time">10:00 AM - 10:30 AM</div>
+        <div class="blitz-link">Not generated</div>
         <div class="bullet-time">10:45 AM - 11:00 AM</div>
+        <div class="bullet-link">Not generated</div>
       
         <div class="date">March 21, 2024</div>
         <div class="round">Round 4.1</div>
         <div class="teams">Anand, Humpy, Karpov, Fischer</div>
         <div class="blitz-time">10:00 AM - 10:30 AM</div>
+        <div class="blitz-link">Not generated</div>
         <div class="bullet-time">10:45 AM - 11:00 AM</div>
-
+        <div class="bullet-link">Not generated</div>
+      
         <div class="date">March 21, 2024</div>
         <div class="round">Round 4.2</div>
         <div class="teams">Kasparov, Carlsen, Hikaru, MVL</div>
         <div class="blitz-time">10:00 AM - 10:30 AM</div>
+        <div class="blitz-link">Not generated</div>
         <div class="bullet-time">10:45 AM - 11:00 AM</div>
+        <div class="bullet-link">Not generated</div>
       
 
-        <div class="date">March 22, 2024</div>
-        <div class="round">Semi Finals</div>
-        <div class="teams">TBA</div>
-        <div class="blitz-time">10:00 AM - 10:30 AM</div>
-        <div class="bullet-time">10:45 AM - 11:00 AM</div>
-
-        <div class="date">March 23, 2024</div>
-        <div class="round">Finals</div>
-        <div class="teams">TBA</div>
-        <div class="blitz-time">10:00 AM - 10:30 AM</div>
-        <div class="bullet-time">10:45 AM - 11:00 AM</div>
+        <div class="date, imp-round">March 22, 2024</div>
+        <div class="round, imp-round">Semi Finals</div>
+        <div class="teams, imp-round">TBA</div>
+        <div class="blitz-time, imp-round">10:00 AM - 10:30 AM</div>
+        <div class="blitz-link, imp-round">Not generated</div>
+        <div class="bullet-time, imp-round">10:45 AM - 11:00 AM</div>
+        <div class="bullet-link, imp-round">Not generated</div>
+      
+        <div class="date, imp-round">March 23, 2024</div>
+        <div class="round, imp-round">Finals</div>
+        <div class="teams, imp-round">TBA</div>
+        <div class="blitz-time, imp-round">10:00 AM - 10:30 AM</div>
+        <div class="blitz-link, imp-round">Not generated</div>
+        <div class="bullet-time, imp-round">10:45 AM - 11:00 AM</div>
+        <div class="bullet-link, imp-round">Not generated</div>
       
       </div>
     );
